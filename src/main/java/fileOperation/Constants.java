f72d161642c703d38ba0f4897f479434a59a6dee
RakshaBM <raksha.bm@razorthink.net>
2017-08-10 14:40:10 +0530

    sonarlint changes

---
+++ src/main/java/fileOperation/Constants.java
@@ -0,0 +1,8 @@
+package fileOperation;
+
+public class Constants {
+	public static final String MACHINEID = "MACHINEID";
+	public static final String KEYWORDS = "kEYWORDS";
+	public static final String BHPRO = "BHPRO";
+
+}
--- src/main/java/fileOperation/Root.java
+++ src/main/java/fileOperation/Root.java
@@ -2,7 +2,6 @@ package fileOperation;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -10,23 +9,29 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class Root {
 
+	public static Logger logger = Logger.getLogger("Root");
 	static String path;
-	HashMap<String, List<String>> map = new HashMap<>();
+	Map<String, List<String>> map = new HashMap<>();
 	static List<Root> roots = new ArrayList<>();
 	static String target;
 
-	public static void main( String[] args ) throws IOException
+	public static void main( String[] args )
 	{
-		path = "/home/raksha/Downloads";
-		target = "/home/raksha/Downloads";
+		path = args[0];
+		target = args[1];
 		List<File> files = filterFiles(path);
+		if(files.size() == 0){
+			logger.info("No files found.");
+		}
 		store(files);
 	}
 
@@ -44,15 +49,15 @@ public class Root {
 		});
 	}
 
-	private static void display( List<String> list ) throws IOException
+	private static void display( List<String> list )
 	{
-		final HashMap<String, List<String>>[] f = new HashMap[] { null };
+		final Map<String, List<String>>[] f = new HashMap[] { null };
 		roots = list.stream().map(l -> {
 			Root root = new Root();
 			String key = l.split("-")[0].trim();
 			String value = l.split("-")[1].trim();
 
-			if( key.equals("MACHINEID") )
+			if( key.equals(Constants.MACHINEID) )
 				f[0] = root.getMap();
 
 			f[0].putIfAbsent(key, Arrays.asList(value));
@@ -70,34 +75,33 @@ public class Root {
 		creatingExcel(roots);
 	}
 
-	private static void creatingExcel( List<Root> roots ) throws IOException
+	private static void creatingExcel( List<Root> roots )
 	{
 
 		File f = checkFile(target);
 		XSSFWorkbook workbook = new XSSFWorkbook();
-		List<String> sheets = new ArrayList<>();
 		final XSSFSheet[] s = { null };
 		for( Root root : roots )
 		{
-			HashMap<String, List<String>> m = root.getMap();
-			if( m.values().isEmpty() )
+			Map<String, List<String>> map = root.getMap();
+			if( map.values().isEmpty() )
 			{
 				continue;
 			}
 
 			Row h;
 			AtomicInteger numberOfRows;
-			if( m.containsKey("MACHINEID") )
+			if( map.containsKey(Constants.MACHINEID) )
 			{
-				s[0] = workbook.getSheet(m.get("MACHINEID").get(0));
+				s[0] = workbook.getSheet(map.get(Constants.MACHINEID).get(0));
 				if( s[0] == null )
 				{
-					s[0] = workbook.createSheet(m.get("MACHINEID").get(0));
+					s[0] = workbook.createSheet(map.get(Constants.MACHINEID).get(0));
 					System.out.println("created sheet " + s[0].getSheetName());
 				}
 
 			}
-			if( m.containsKey("kEYWORDS") )
+			if( map.containsKey(Constants.KEYWORDS) )
 			{
 				System.out.println("using " + s[0].getSheetName());
 				if( s[0].getRow(0) == null )
@@ -105,9 +109,7 @@ public class Root {
 					h = s[0].createRow(0);
 					AtomicInteger i = new AtomicInteger(0);
 					Row finalH = h;
-					Arrays.asList(m.get("kEYWORDS").get(0).split("/")).forEach(lon -> {
-						finalH.createCell(i.getAndIncrement()).setCellValue(lon);
-					});
+					Arrays.asList(map.get(Constants.KEYWORDS).get(0).split("/")).forEach(lon -> finalH.createCell(i.getAndIncrement()).setCellValue(lon));
 					System.out.println("created new header");
 				}
 				else
@@ -124,10 +126,10 @@ public class Root {
 						it++;
 					}
 					System.out.println(values.toString());
-					Arrays.asList(m.get("kEYWORDS").get(0).split("/")).forEach(lon -> {
-						if( !values.contains(lon) )
+					Arrays.asList(map.get(Constants.KEYWORDS).get(0).split("/")).forEach(keyword -> {
+						if( !values.contains(keyword) )
 						{
-							finalH.createCell(numberOfCells.getAndIncrement()).setCellValue(lon);
+							finalH.createCell(numberOfCells.getAndIncrement()).setCellValue(keyword);
 
 						}
 					});
@@ -142,11 +144,11 @@ public class Root {
 			while( itr.hasNext() )
 			{
 				Cell cell = itr.next();
-				if( m.containsKey(cell.getStringCellValue()) )
+				if( map.containsKey(cell.getStringCellValue()) )
 				{
 					index = cell.getColumnIndex();
 					Cell cel = h.createCell(index);
-					cel.setCellValue(m.get(cell.getStringCellValue()).get(0));
+					cel.setCellValue(map.get(cell.getStringCellValue()).get(0));
 				}
 			}
 		}
@@ -154,28 +156,49 @@ public class Root {
 		{
 			workbook.write(outputStream);
 		}
+		catch( IOException e )
+		{
+			e.printStackTrace();
+		}
 	}
 
-	private static File checkFile( String target ) throws IOException
+	private static File checkFile( String target )
 	{
-		if( Files.list(Paths.get(target)).anyMatch(file -> file.getFileName().toString().contains("final")) )
-			return Paths.get(target + "/final.xlsx").toFile();
-		return Files.createFile(Paths.get(target + "/final.xlsx")).toFile();
+		try
+		{
+			if( Files.list(Paths.get(target)).anyMatch(file -> file.getFileName().toString().contains("final")) )
+				return Paths.get(target + "/final.xlsx").toFile();
+			return Files.createFile(Paths.get(target + "/final.xlsx")).toFile();
+		}
+		catch( IOException e )
+		{
+			e.printStackTrace();
+		}
+		return null;
 	}
 
-	private static List<File> filterFiles( String path ) throws IOException
+	private static List<File> filterFiles( String path )
 	{
-		return Files.list(Paths.get(path)).map(path1 -> path1.toFile()).filter(file -> file.getName().contains("BHPRO"))
-				.collect(Collectors.toList());
+		try
+		{
+			return Files.list(Paths.get(path)).map(Path::toFile).filter(file -> file.getName().contains(Constants.BHPRO))
+					.collect(Collectors.toList());
+		}
+		catch( IOException e )
+		{
+			e.printStackTrace();
+		}
+		return null;
 	}
 
-	public HashMap<String, List<String>> getMap()
+	public Map<String, List<String>> getMap()
 	{
 		return map;
 	}
 
-	public void setMap( HashMap<String, List<String>> map )
+	public void setMap( Map<String, List<String>> map )
 	{
 		this.map = map;
 	}
+
 }
